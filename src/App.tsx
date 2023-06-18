bd2706a7df0197ace56a4291cab64b5bb2a3f094
iliya.ahmadi <iliya.ahmadi@partsoftware.com>
2023-06-18 10:14:37 +0330

    initial: create react-ts app structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>یووووووووووووو</h2>
+    </>
+  )
+}
+
+export default App
