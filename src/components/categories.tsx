a3764c9e041ab670302d509ef39c4036b8bdeec7
iliya.ahmadi <iliya.ahmadi@partsoftware.com>
2023-06-20 16:36:26 +0330

    feat : add body for categories component

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -7,18 +7,127 @@ function categories() {
         style={{
           backgroundImage: 'url("src/assets/img/dj.jpg")',
         }}
-        className="categories__head head"
+        className="categories__head categories-head"
       >
         <div className="categories__head--blur"></div>
-        <div className="head__btn">
+        <div className="categories-head__btn">
           <img src="src/assets/icons/play.svg" alt="play button" />
         </div>
-        <div className="head__title">
+        <div className="categories-head__title">
           <p>اهنگ های تو</p>
           <b>روی ریپیت</b>
         </div>
       </div>
-      <div className="categories__items"></div>
+      <div className="categories__body categories-body">
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/rap.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>رپ</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/rock.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>راک</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/metal.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>متال</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/workout.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>ورزش</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/beach.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>ساحلی</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/rap.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>رپ</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/rock.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>راک</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/chill.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>چیل</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/chill.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>چیل</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/traditional.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>محلی</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/Artist.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>ریمیکس</p>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("src/assets/img/beach.jpg")',
+          }}
+          className="categories-body__item"
+        >
+          <div className="categories-body__item--blur"></div>
+          <p>ساحلی</p>
+        </div>
+      </div>
     </div>
   );
 }
