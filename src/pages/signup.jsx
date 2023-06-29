a6ccb6438809cbd63f4264cea9bcca864cbc1216
iliya.ahmadi <iliya.ahmadi@partsoftware.com>
2023-06-29 17:24:09 +0330

    feat : add signup page

---
+++ src/pages/signup.jsx
@@ -0,0 +1,12 @@
+import '../styles/pages/_signup.scss';
+import SignupForm from '../components/signupForm';
+
+function signup() {
+  return (
+    <div className="signup">
+      <SignupForm />
+    </div>
+  );
+}
+
+export default signup;
