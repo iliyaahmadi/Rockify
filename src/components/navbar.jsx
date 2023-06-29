2e0e355f2fdda6b47e980c56ddadd680b98bcc1a
iliya.ahmadi <iliya.ahmadi@partsoftware.com>
2023-06-29 13:19:21 +0330

    feat : add login btn

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,31 @@
+import { Link } from 'react-router-dom';
 import '../styles/components/_navbar.scss';
+import { useState } from 'react';
 
 function navbar(props) {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <div className="navbar">
       <p className="navbar__greeting">{props.title}</p>
-      <img
-        className="navbar__notification"
-        src="src\assets\icons\bell.svg"
-        alt="notifications"
-      />
-      <img
-        className="navbar__profile"
-        src="src\assets\img\profile.jpg"
-        alt="profile picture"
-      />
+      {isLogin ? (
+        <img
+          className="navbar__notification"
+          src="src\assets\icons\bell.svg"
+          alt="notifications"
+        />
+      ) : null}
+      {isLogin ? (
+        <img
+          className="navbar__profile"
+          src="src\assets\img\profile.jpg"
+          alt="profile picture"
+        />
+      ) : (
+        <Link to="/login">
+          <div className="navbar__button">ورود</div>
+        </Link>
+      )}
     </div>
   );
 }
