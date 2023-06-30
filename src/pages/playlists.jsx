9ed0cc688076018e9af7ab7a707ec0cbb6e3cbc2
iliyaahmadi <iliyahmdi@yahoo.com>
2023-06-30 13:52:44 +0330

    style : fix padding

--- src/pages/playlists.jsx
+++ src/pages/playlists.jsx
@@ -1,10 +1,14 @@
 import '../styles/pages/_playlists.scss';
 import Navbar from '../components/navbar';
+import PlaylistBox from '../components/playlistBox';
 
 function playlists() {
   return (
     <div className="playlists">
       <Navbar title="لیست های پخش" />
+      <div className="playlists__wrapper">
+        <PlaylistBox />
+      </div>
     </div>
   );
 }
